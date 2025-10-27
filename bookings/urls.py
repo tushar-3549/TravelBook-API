41a28d90da0e1137efaf2440ee7bf2682783ad72
tushar-3549 <tushardiu3549@gmail.com>
2025-10-27 14:02:18 +0600

    booking mine fixed

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
 from .views import QuoteView, BookingCreateView, BookingDetailView, MyBookingsView
-urlpatterns=[path('quote/', QuoteView.as_view()), path('', BookingCreateView.as_view()), path('<str:code>/', BookingDetailView.as_view()), path('mine/', MyBookingsView.as_view())]
+
+urlpatterns = [
+    path('quote/', QuoteView.as_view(), name='quote'),
+    path('mine/', MyBookingsView.as_view(), name='my-bookings'), 
+    path('', BookingCreateView.as_view(), name='booking-create'),
+    path('<str:code>/', BookingDetailView.as_view(), name='booking-detail'),
+]
